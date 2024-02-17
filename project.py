6f789382b48a7b2e5b513d1660dcd6325e62ffa8
soumika315 <79858317+soumika315@users.noreply.github.com>
2024-02-17 15:51:56 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,10 @@
+
+my_list = [5, 2, 8, 1, 9]
+
+    # Reverse sorting using sort() method
+my_list.sort(reverse=True)
+print("Reverse sorted list:", my_list)
+
+    # Copying list using copy() method
+copied_list = my_list.copy()
+print("Copied list:", copied_list)
